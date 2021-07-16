7188a2c26d324bd485a377c26de3b9b97cb78516
george <tang19970214@gmail.com>
2021-07-16 13:05:51 +0800

    upload

--- components/pages/FormFooter.vue
+++ components/pages/FormFooter.vue
@@ -4,24 +4,24 @@
       <div class="w-full px-3 py-4 box-border fixed bottom-0 left-0 bg-white z-20">
         <div class="w-full flex items-center justify-around">
           <!-- 上一個類型 -->
-          <button class="bg-primary px-3 py-1 rounded-sm flex items-center justify-center">
+          <button class="bg-primary px-3 py-1 rounded-sm flex items-center justify-center" @click="formBtn('prev')">
             <font-awesome-icon class="text-white text-xs mr-1" :icon="['fas', 'chevron-left']" />
             <strong class="text-sm text-white">上一個類型</strong>
           </button>
 
           <!-- 下一個類型 -->
-          <button class="bg-primary px-3 py-1 rounded-sm flex items-center justify-center">
+          <button class="bg-primary px-3 py-1 rounded-sm flex items-center justify-center" @click="formBtn('next')">
             <strong class="text-sm text-white">下一個類型</strong>
             <font-awesome-icon class="text-white text-xs ml-1" :icon="['fas', 'chevron-right']" />
           </button>
 
           <!-- 暫存 -->
-          <button class="bg-primary px-3 py-1 rounded-sm flex items-center justify-center">
+          <button class="bg-primary px-3 py-1 rounded-sm flex items-center justify-center" @click="formBtn('save')">
             <strong class="text-sm text-white">暫存</strong>
           </button>
 
           <!-- 送出 -->
-          <button class="bg-primary px-3 py-1 rounded-sm flex items-center justify-center">
+          <button class="bg-primary px-3 py-1 rounded-sm flex items-center justify-center" @click="formBtn('send')">
             <strong class="text-sm text-white">送出</strong>
           </button>
         </div>
